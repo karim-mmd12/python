b1aa876e643489b8acaaf8747bf588daf3c51e5d
Abdul karim Yassin <karim.mmd12@gmail.com>
2021-05-08 10:31:54 +0200

    printing the board with numbers

--- game.py
+++ game.py
@@ -1,30 +1,42 @@
 from player import Player
 
 
-class Quoridor():
-    @classmethod
-    def print_board_with_numbers(cls):
+class Game():
+    def __init__(self, board_size=10):
+        self.board = [' ' for i in range(board_size*board_size)]
+
+    @staticmethod
+    def print_board_with_numbers():
+        number_board = [['0'+str(j) if j < 10 else str(j) for j in range(i*10, (i+1)*10)]
+                        for i in range(10)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def empty_squares(self):
+        return ' '
+
+    def draw(self):
         pass
 
+    def play(self, p1, p2):
 
-def play(game, p1, p2):
+        Game.print_board_with_numbers()
 
-    Quoridor.print_board_with_numbers()
+        player_name = 'p2'
+        # check if empty squares around player only-no walls or other player
+        while self.empty_squares():
+            if p1.name == player_name:
+                position = p1.get_position()
+            else:
+                player_name = 'p2'
+                position = p2.get_position()
 
-    player_name = 'p1'
-    # check if empty squares around player only-no walls or other player
-    while game.empty_squares():
-        if player_name == 'p1':
-            position = p1.move()
-        else:
-            position = p2.move()
-
-        print(f"{player_name} moves to position {position}")
-        game.draw()
+            print(f"{player_name} moves to position {position}")
+            self.draw()
 
 
 if __name__ == '__main__':
-    p1 = Player('p1')
-    p2 = Player('p2')
-    q = Quoridor()
-    play(q, p1, p2)
+    p1 = Player('p1', 95)
+    p2 = Player('p2', 5)
+    q = Game()
+    q.play(p1, p2)
--- player.py
+++ player.py
@@ -1,2 +1,14 @@
 class Player():
-    pass
+    def __init__(self, name, position):
+        self.name = name
+        self.starting_position = position,
+        self.position = self.starting_position
+
+        print(f"{self.name}{self.starting_position}")
+
+    def get_position(self):
+        available_position = 0
+        return available_position
+
+    def move(self, position):
+        self.position = position
