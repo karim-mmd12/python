7e2e2a9070b7d9f15e34e4bf0418ece89d78b37c
Abdul karim Yassin <karim.mmd12@gmail.com>
2021-06-04 19:58:10 +0200

    adding comments

--- game.py
+++ game.py
@@ -2,18 +2,30 @@ import time
 
 
 class Game():
+    # board size
     B_S = 10
     nb_players = 0
+    # player1 and player2
     players = []
     WALL = '###'
+    # constructor of the class
+    # board_size is 10x10
 
+    # board size is by default 10
     def __init__(self, board_size=10):
         self.board_size = board_size
+        # range(0,99)
+        # i = 0, 1,2,3,4....99
         self.board = [' ' for i in range(self.board_size*self.board_size)]
         self.winner = None
 
     @classmethod
     def print_board_with_numbers(cls):
+        # for i in range(10)
+        # for j in range(20, 30) j= '0','1',2,3,4,5....9
+        # 10,11,12.....19
+        # 20,21
+        # number_board = [[0,1,2,3,....9],[10,11,12,13,14...19],[20,21,....29]...[90,91,92,....99]]
         number_board = [[str(j)
                          for j in range(i*cls.B_S, (i+1)*cls.B_S)] for i in range(cls.B_S)]
         for row in number_board:
@@ -31,6 +43,8 @@ class Game():
         return self.board[position[0]] == Game.WALL
 
     def empty_squares(self):
+        # return true or false
+        # board= ['###', 'p1', ' ', 'p2' ]
         return ' ' in self.board
 
     def available_moves(self):
@@ -45,10 +59,12 @@ class Game():
             count += 1
 
     def play(self):
+        # board with numbers
         Game.print_board_with_numbers()
         x_player, y_player = Game.players[0:3]
         player_name = 'P1'
         # check if still empty squares in board
+        # ['p1','p2','###','###',]
         while self.empty_squares():
             if x_player.name == player_name:
                 position = x_player.get_position(self)
--- main_bachir.py
+++ main_bachir.py
@@ -1,10 +1,15 @@
+# we need to use the Game class, we have to import the class from the file name game.py
 from game import Game
 from player import Player
 
 if __name__ == '__main__':
+    # object instance
     game = Game()
+    # we created 2 players called p1 and p2. player1 at pos 95 and player2 at pos 5.
     Player(game, 'P1', (95,))
     Player(game, 'P2', (5,))
+    # board: list of length 100.
     game.board[95] = 'P1'
     game.board[5] = 'P2'
+    # here we start the game
     game.play()
